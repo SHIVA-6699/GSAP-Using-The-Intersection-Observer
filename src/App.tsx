adb410261a53b0d0e910847468a860d1470b4c61
SHIVA-6699 <pillashivakrishna6@gmail.com>
2024-02-12 09:31:26 +0530

    GSAP with Recat Intersection Observer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
+import logo from './assets/img1.jpg';
+import { TweenMax, Power3 } from 'gsap';
 
 function App() {
+  let Image = useRef<HTMLImageElement | null>(null);
+  let Text = useRef<HTMLParagraphElement | null>(null);
+
+  useEffect(() => {
+    if (Image.current && Text.current) {
+      TweenMax.to(
+        Image.current,
+        0.8,
+        {
+          opacity: 1,
+          y: -20,
+          ease: Power3.easeInOut
+        }
+      );
+
+      TweenMax.to(
+        Text.current,
+        0.8,
+        {
+          opacity: 1,
+          y: -20,
+          ease: Power3.easeInOut,
+          delay: 2
+        }
+      );
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className='App-logo' ref={Image} src={logo} alt="renderimage" />
+      <p className='App-header' ref={Text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum quis molestiae vero aut ducimus a ad in voluptatum eum explicabo ipsa, voluptates temporibus dolore laborum, repudiandae, sint quidem id similique?</p>
     </div>
   );
 }
